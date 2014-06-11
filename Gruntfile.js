5371718a8d0e3e84ecb77156691a7b5fe3cad693
spacerockzero <jakob@jakobanderson.com>
2014-06-11 16:25:38 -0600

    working 10-run batches

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,51 @@
 'use strict';
 
+/* globals require */
+
 var grunt = require('grunt');
 
 /* globals grunt */
 var currentSession = 'USYSD417EFF65E8F9BEB2113094AF748A277_idses-refa03.a.fsglobal.net';
 var options = {
   cookie: 'fssessionid='+currentSession,
-  timeout: 120,
-  verbose: true,
-  debug: true
+  timeout: 500,
+  viewport: '1280x1024'
 };
 
 grunt.initConfig( {
+
   phantomas : {
+
     viewerPage: {
       options: {
-        indexPath: './phantomas/',
-        numberOfRuns: 1,
+        indexPath: './results/viewerPage/',
+        numberOfRuns: 10,
         options: options,
-        url: 'http://localhost:5000/images/6218370?cc=UNKNOWN'
+        url: 'http://localhost:5000/images/6218370?cc=UNKNOWN',
+        group: {
+          criticalPerf: [
+            // 'timeToFirstByte',
+            // 'timeToLastByte',
+            // 'timeToFirstCss',
+            // 'timeToFirstJs',
+            // 'timeToFirstImage',
+            // 'fastestResponse',
+            // 'slowestResponse',
+            'onDOMReadyTime',
+            'onDOMReadyTimeEnd',
+            'windowOnLoadTime',
+            'windowOnLoadTimeEnd',
+            'httpTrafficCompleted',
+            'timeBackend',
+            'timeFrontend',
+            'cookiesSent'
+          ]
+        }
       }
     }
+
   }
+
 });
 
 // load tasks
