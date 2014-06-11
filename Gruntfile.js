f098ac46d6ec1670a9b334b166f8860a375534f8
spacerockzero <jakob@jakobanderson.com>
2014-06-11 14:59:53 -0600

    working perf test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,24 @@
 'use strict';
 
+var grunt = require('grunt');
+
 /* globals grunt */
 var currentSession = 'USYSD417EFF65E8F9BEB2113094AF748A277_idses-refa03.a.fsglobal.net';
 var options = {
-  cookie: 'fssessionid='+currentSession+';domain=/'
+  cookie: 'fssessionid='+currentSession,
+  timeout: 120,
+  verbose: true,
+  debug: true
 };
 
 grunt.initConfig( {
-  phantomas: {
-    gruntSite : {
-      options : {
-        indexPath : './phantomas/',
-        options   :  options,
-        url       : 'http://localhost:5000/images/6218370?cc=UNKNOWN',
-        buildUi   : true
+  phantomas : {
+    viewerPage: {
+      options: {
+        indexPath: './phantomas/',
+        numberOfRuns: 1,
+        options: options,
+        url: 'http://localhost:5000/images/6218370?cc=UNKNOWN'
       }
     }
   }
